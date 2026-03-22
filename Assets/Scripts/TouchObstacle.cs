095b6cafaafaaa4155ae9701fee2597d4c5caa13
Joshua Lai <joshua.lai@sjsu.edu>
2026-03-22 11:20:00 -0700

    Added obstacles (respawns for now)

--- Assets/Scripts/TouchObstacle.cs
+++ Assets/Scripts/TouchObstacle.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Respawn : MonoBehaviour
+public class TouchObstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
